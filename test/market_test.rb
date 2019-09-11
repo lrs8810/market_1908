855909222b0bd62e7e159d215e27966b40aab5f4
Laura Schulz <lrs8810@gmail.com>
2019-09-11 13:16:28 -0600

    Add iteration 1

---
+++ test/market_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+require './lib/market'
+
+class MarketTest < Minitest::Test
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")  
+  end
+end
