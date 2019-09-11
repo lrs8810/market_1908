72964fcc8c01e2e94515c6f18a110595b92d3728
Laura Schulz <lrs8810@gmail.com>
2019-09-11 13:36:25 -0600

    Add tests and methods for iteration 2

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,26 @@
 class Market
-  
+  attr_reader :name, :vendors
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.inject([]) do |vendor_names, vendor|
+      vendor_names << vendor.name
+    end
+  end
+
+  def vendors_that_sell(item)
+    vendor_sells_item = []
+    @vendors.each do |vendor|
+      vendor_sells_item << vendor if vendor.check_stock(item) > 0
+    end
+    vendor_sells_item
+  end
 end
