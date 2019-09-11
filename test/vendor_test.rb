5301f4adda515a326db3beb2d2843fd0865f71ec
Laura Schulz <lrs8810@gmail.com>
2019-09-11 14:14:43 -0600

    Refactor vendor tests

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -16,19 +16,26 @@ class VendorTest < Minitest::Test
     assert_equal ({}), @vendor.inventory
   end
 
-  def test_check_stock_of_inventory
+  def test_inventory_starts_without_any_items
     assert_equal 0, @vendor.check_stock("Peaches")
+  end
 
+  def test_check_stock_of_item
     @vendor.stock("Peaches", 30)
+
     assert_equal 30, @vendor.check_stock("Peaches")
 
     @vendor.stock("Peaches", 25)
-    assert_equal 55, @vendor.check_stock("Peaches")
 
-    @vendor.stock("Tomatoes", 12)
-    expected = {"Peaches"=>55, "Tomatoes"=>12}
-    assert_equal expected, @vendor.inventory
+    assert_equal 55, @vendor.check_stock("Peaches")
   end
 
-  
+  def test_inventory_after_adding_multiple_items
+    @vendor.stock("Peaches", 30)
+    @vendor.stock("Peaches", 25)
+    @vendor.stock("Tomatoes", 12)
+    expected = {"Peaches"=>55, "Tomatoes"=>12}
+
+    assert_equal expected, @vendor.inventory
+  end
 end
